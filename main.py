7a24ce9bafeffa369670aaadbdd98bf705ff80d4
Bihe Dong <dongbihe1997@gmail.com>
2024-05-10 14:03:50 +0200

    Change python to .0

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import subprocess
 
+
 def run_flask():
     subprocess.Popen(["gunicorn", "app:app"])
 
+
 def run_streamlit():
     subprocess.Popen(["streamlit", "run", "streamlit_app.py"])
+
 
 if __name__ == "__main__":
     run_flask()
