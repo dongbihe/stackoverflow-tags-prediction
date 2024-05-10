cdb3fa2c520b8e1e6e32bcb3f1622db8281efdd5
Bihe Dong <dongbihe1997@gmail.com>
2024-05-10 15:56:52 +0200

    Add new workflow

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def run_flask():
 
 
 def run_streamlit():
-    subprocess.Popen(["streamlit", "run", "streamlit_app.py"])
+    subprocess.Popen(["python", "-m", "streamlit", "run", "streamlit_app.py", "--server.port", "8001", "--server.address", "0.0.0.0"])
 
 
 if __name__ == "__main__":
