a3c87c1f84922d182777f2bac6aac834dd423eac
Bihe Dong <dongbihe1997@gmail.com>
2024-05-13 11:09:43 +0200

    Black

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ def run_flask():
 
 
 def run_streamlit():
-    subprocess.Popen(["python", "-m", "streamlit", "run", "streamlit_app.py", "--server.port", "8001", "--server.address", "0.0.0.0"])
+    subprocess.Popen(
+        [
+            "python",
+            "-m",
+            "streamlit",
+            "run",
+            "streamlit_app.py",
+            "--server.port",
+            "8001",
+            "--server.address",
+            "0.0.0.0",
+        ]
+    )
 
 
 if __name__ == "__main__":
